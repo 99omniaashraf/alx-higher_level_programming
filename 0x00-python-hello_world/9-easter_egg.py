ab48c352456ae04672b37fe9920ef570602c4fe2
99omniaashraf <omniaashraf249@gmail.com>
2023-10-30 15:52:15 -0700

    Copy - Cut - Paste

--- 0x00-python-hello_world/9-easter_egg.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-print(f"Beautiful is better than ugly.")
-print(f"Explicit is better than implicit.")
-print(f"Simple is better than complex.")
-print(f"Complex is better than complicated.")
