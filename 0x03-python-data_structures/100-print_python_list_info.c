9ae32b7e165cf082459410c79cf63741706e9da7
99omniaashraf <omniaashraf249@gmail.com>
2023-11-06 15:49:28 -0800

    CPython #0: Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -3,23 +3,21 @@
 /**
  * print_python_list_info - gives data of the PyListObject
  * @p: the PyObject
+ * Return: no return
  */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize size = 0;
+	int size = 0;
 	int i = 0;
 
-	if (PyList_CheckExact(p))
-	{
-		size = PyList_Size(p);
-		printf("[*] Size of the Python List = %zd\n", size);
-		printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	size = Py_SIZE(p);
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 
-		while (i < size)
-		{
-			printf("Element %d: %s\n",
-					i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
-			i++;
-		}
+	while (i < size)
+	{
+		printf("Element %d: %s\n",
+				i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+		i++;
 	}
 }
--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import ctypes
 
 lib = ctypes.CDLL('./libPyList.so')
