367a4fe603bd30cd461d725605d5f54771899caf
99omniaashraf <omniaashraf249@gmail.com>
2023-11-21 12:50:15 -0800

    Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -12,30 +12,35 @@ class Node:
             data (int): The data of the new node.
             next_node (Node): The next node of the new Node.
         """
-        self.data = data
-        self.next_node = next_node
+        if type(data) is not int:
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = data
+        self.__next_node = next_node
 
     @property
     def data(self):
         """Get/set the data of the Node."""
-        return (self.__data)
+        return self.__data
 
     @data.setter
     def data(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("data must be an integer")
-        self.__data = value
+        else:
+            self.__data = value
 
     @property
     def next_node(self):
         """Get/set the data of the Node."""
-        return (self.__next_node)
+        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        if not isinstance(value, Node) and value is not None:
+        if type(value) is not Node and value is not None:
             raise TypeError("next_node must be a Node object")
-        self.__next_node = value
+        else:
+            self.__next_node = value
 
 
 class SinglyLinkedList:
