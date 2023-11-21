bff20de3f4575ab0276fb2b95487627f49460653
99omniaashraf <omniaashraf249@gmail.com>
2023-11-21 09:33:54 -0800

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,7 +17,7 @@ class Square:
     @property
     def size(self):
         """Get/set the current size of the square."""
-        return (self.size)
+        return (self.__size)
 
     @size.setter
     def size(self, value):
