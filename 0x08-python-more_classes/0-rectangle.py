785c3d411996d8a7a8b813b3a6e41741780079dd
99omniaashraf <omniaashraf249@gmail.com>
2023-11-27 07:45:55 -0800

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Rectangle Class.
+
+
+This module contains an empty class that defines a rectangle.
+
+"""
+
+
+class Rectangle:
+    """Defines the blueprint of a rectangle"""
+
+    pass
