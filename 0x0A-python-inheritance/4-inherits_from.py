185c65e13dc68c59f9ee1464a4a8e38a8c15ca43
99omniaashraf <omniaashraf249@gmail.com>
2023-12-04 02:36:40 -0800

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module 4-inherits_from"""
+
+
+def inherits_from(obj, a_class):
+    """Determines if obj is an instance of a class
+    that inherited from a_class.
+
+
+    Args:
+        obj: object to look at.
+        a_class: class to evaluate.
+
+
+    Returns: True or False
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
