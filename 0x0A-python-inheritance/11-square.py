6372d7101a2318e667a2dbbeba5911ffe9b74468
99omniaashraf <omniaashraf249@gmail.com>
2023-12-06 08:38:44 -0800

    Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module 11-square"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square."""
+    def __init__(self, size):
+        """Initializes a Square.
+
+
+        Args:
+            size: size of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return str("[Square] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        """Computes the area of a Square instance.
+        Overwrites the area() method from Rectangle.
+        """
+        return self.__size ** 2
