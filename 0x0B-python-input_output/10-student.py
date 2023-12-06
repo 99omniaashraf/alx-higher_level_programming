3f2f89aa5fac879a27e2f503e2cc7c4cc91d93c7
99omniaashraf <omniaashraf249@gmail.com>
2023-12-05 16:55:52 -0800

    python-input_output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -19,7 +19,10 @@ class Student:
 
         Return: dictionary.
         """
-        if not attrs:
-            return self.__dict__
-        return ({key: value for key, value in self.__dict__.items()
-                if key in attrs})
+        my_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__.copy()
