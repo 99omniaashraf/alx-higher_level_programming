1e6ebe8b1036f87da1a9dc35ce4285f34fe1c846
99omniaashraf <omniaashraf249@gmail.com>
2023-12-05 16:58:46 -0800

    python-input_output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,11 +13,13 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
+
     triangle = [[1]]
+
     for i in range(1, n):
         row - [1]
         for i in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
-    return my_list
+    return triangle
