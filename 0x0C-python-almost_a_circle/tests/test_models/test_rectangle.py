62e5baf7d4bf6ef4fc941aa5d07100da4afac239
99omniaashraf <omniaashraf249@gmail.com>
2023-12-10 08:49:35 -0800

    Almost a circle

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Unittest rectangle."""
+
+import unittest
+import io
+import contextlib
+import models.base import Base
+import models.rectangle import Rectangle
+import models.square import Square
