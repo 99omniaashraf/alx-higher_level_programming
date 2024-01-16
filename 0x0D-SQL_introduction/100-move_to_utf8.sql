d05454f86d2c50dd33b5a380b8a70183df52fbd5
99omniaashraf <omniaashraf249@gmail.com>
2024-01-16 15:54:54 -0800

    SQL_introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,6 @@
 -- Convert the database to UTF-8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Use the specified database
-USE hbtn_0c_0;
-
 -- Convert the table to UTF-8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
