d5d9fca7813c60f90d20db304275d368db9585fe
99omniaashraf <omniaashraf249@gmail.com>
2024-01-17 15:31:55 -0800

    SQL_more_queries

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,12 @@
--- List all shows without the genre Comedy
-SELECT tv_shows.title
+-- use left JOIN and subquery
+-- Execute: cat 101-not_a_comedy.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+SELECT title 
 FROM tv_shows
-LEFT JOIN tv_genres ON tv_shows.id = tv_genres.show_id AND tv_genres.name = 'Comedy'
-WHERE tv_genres.show_id IS NULL
-ORDER BY tv_shows.title ASC;
-
+WHERE title NOT IN (SELECT tv_shows.title
+	FROM tv_shows
+	LEFT JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+	LEFT JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+	WHERE tv_genres.name = 'Comedy')
+ORDER BY 1 ASC;
