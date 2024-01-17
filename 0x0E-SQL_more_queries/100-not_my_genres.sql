1b5aa70a10e3e030e885597455a6ea7507fbb610
99omniaashraf <omniaashraf249@gmail.com>
2024-01-17 15:12:14 -0800

    SQL_more_queries

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- Select LEFT JOIN with where clausule
+SELECT name
+FROM tv_genres
+WHERE name
+NOT IN (SELECT a.name FROM tv_genres a
+	RIGHT JOIN tv_show_genres a
+	ON a.id = b.genre_id
+	RIGHT JOIN tv_shows C
+	ON b.show_id = c.id
+	WHERE c.title = 'Dexter')
+ORDER BY 1 ASC;
---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- Function SUM
+SELECT
+tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+LEFT JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY 2 DESC;
