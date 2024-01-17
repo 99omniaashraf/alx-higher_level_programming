e6edc055b15d684255156abb2b4c4325b415b187
99omniaashraf <omniaashraf249@gmail.com>
2024-01-17 15:23:33 -0800

    SQL_more_queries

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,18 +1,7 @@
---Use LEFT JOIN and subquery
-SELECT DISTINCT `title`
-  FROM `tv_shows` AS t
-       LEFT JOIN `tv_show_genres` AS s
-       ON s.`show_id` = t.`id`
+-- List all shows without the genre Comedy
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_genres ON tv_shows.id = tv_genres.show_id AND tv_genres.name = 'Comedy'
+WHERE tv_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC;
 
-       LEFT JOIN `tv_genres` AS g
-       ON g.`id` = s.`genre_id`
-       WHERE t.`title` NOT IN
-             (SELECT `title`
-                FROM `tv_shows` AS t
-	             INNER JOIN `tv_show_genres` AS s
-		     ON s.`show_id` = t.`id`
-
-		     INNER JOIN `tv_genres` AS g
-		     ON g.`id` = s.`genre_id`
-		     WHERE g.`name` = "Comedy")
- ORDER BY `title`;
