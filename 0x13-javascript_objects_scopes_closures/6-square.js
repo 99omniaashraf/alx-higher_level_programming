365a3b89fd4de071a0bfc0c49aa2efd4ad28e31b
99omniaashraf <omniaashraf249@gmail.com>
2024-02-13 10:12:33 -0800

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// class Square that defines a square and
-// inherits from Square of 5-square.js
+// class Square that defines a square and inherits from Square of 5-square.js
 
 const SquareBase = require('./5-square');
 
