c257056b9d3615ac735351ddc1ffea8d2746230d
99omniaashraf <omniaashraf249@gmail.com>
2024-02-13 10:39:21 -0800

    Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,5 +2,10 @@
 // function that returns the reversed version of a list
 
 exports.esrever = function (list) {
-  return list.sort(() => 1);
+  const reversedList = [];
+  const last = list.length - 1;
+  for (let i = last; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return (reversedList);
 };
