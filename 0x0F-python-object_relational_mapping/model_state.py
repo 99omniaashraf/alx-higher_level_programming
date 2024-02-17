5aac17a6dfe431d17d6895dc44c3da034be851e8
99omniaashraf <omniaashraf249@gmail.com>
2024-02-17 09:34:31 -0800

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-""" Defines State ORM object"""
+"""
+Class definition of a State with inheritage from Base
+"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from model_city import City
 
 Base = declarative_base()
 
 
 class State(Base):
-    """State ORM class"""
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
+    """
+    State Class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
