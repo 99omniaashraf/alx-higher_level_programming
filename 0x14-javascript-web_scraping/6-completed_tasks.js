9fdf31ad2336ea8e30d02ad1785cda0e704509b9
99omniaashraf <omniaashraf249@gmail.com>
2024-03-27 00:14:06 +0200

    How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Computes the number of tasks completed by user id.
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, { json: true }, (err, res, body) => {
+  if (err) console.log(err);
+  const results = {};
+  for (const task of body) {
+    if (task.completed) {
+      results[task.userId] = (results[task.userId] || 0) + 1;
+    }
+  }
+  console.log(results);
+});
