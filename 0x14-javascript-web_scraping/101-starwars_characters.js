d75dba29f1a9784237e220a0b4081832fec5a32d
99omniaashraf <omniaashraf249@gmail.com>
2024-03-27 01:58:18 +0200

    Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Prints all characters of a Star Wars movie
+
+const request = require('request');
+const util = require('util');
+const id = process.argv[2];
+
+const prequest = util.promisify(request);
+
+(async () => {
+  try {
+    const body = (await prequest(`https://swapi-api.alx-tools.com/api/films/${id}`, { json: true })).body;
+    for (const url of body.characters) {
+      const body = (await prequest(url, { json: true })).body;
+      console.log(body.name);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+})();
