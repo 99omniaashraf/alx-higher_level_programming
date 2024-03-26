e5e286314e3398abac1e3b1eb36d63f2ac68ff6a
99omniaashraf <omniaashraf249@gmail.com>
2024-03-26 22:41:45 +0200

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Display the status code of a GET request.
+
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
